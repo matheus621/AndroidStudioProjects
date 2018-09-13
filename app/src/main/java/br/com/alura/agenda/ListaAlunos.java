6ce7050e95a4bf388c1a3346e7770550b879215b
Matheus Cardoso de Souza <matheus.souza@maxima.local>
2018-09-13 18:19:00 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/agenda/ListaAlunos.java
@@ -0,0 +1,21 @@
+package br.com.alura.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+
+        String[] alunos = {"Matheus Cardoso","Carlos Souza","Amanda Ribeiro","Isabela Gusmão"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+}
